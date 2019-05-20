416825e27cc62238b8dfe79eeec3785d5be51f17
manu-sh <s3gmentationfault@gmail.com>
2019-05-20 22:22:44 +0200

    fix fragment parsing

--- http_normalizer/include/private/http_normalizer/private/http_tokenizer.hpp
+++ http_normalizer/include/private/http_normalizer/private/http_tokenizer.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <cstdint>
 #include <vector>
-#include <forward_list>
 #include <string>
 #include <functional>
 #include <sstream>
@@ -17,10 +16,10 @@ class http_tokenizer {
 
 http_tokenizer::http_tokenizer(const std::string &http_url): m_vct(parts::LENGTH) {
 
-	using std::vector, std::string, std::forward_list, std::function, std::pair;
+	using std::vector, std::string, std::function, std::pair;
 	using functions = function<bool(const string &, size_t &, uint_fast8_t &)>;
 
-	const static forward_list< functions > prototype
+	const static vector< functions > eval
 	{
 		[] (const string &s, size_t &i, uint_fast8_t &j) { // match proto end
 
@@ -45,13 +44,9 @@ http_tokenizer::http_tokenizer(const std::string &http_url): m_vct(parts::LENGTH
 		}, [] (const string &s, size_t &i, uint_fast8_t &j) { // match port end
 
 			switch (s[i]) {
-				// case '/': j = parts::PATH;     return (i += i+1 < s.length()), true;
-				// case '?': j = parts::QUERY;    return (i += i+1 < s.length()), true;
-				// case '#': j = parts::FRAGMENT; return (i += i+1 < s.length()), true;
-
-				case '/': j = parts::PATH; return true;
-				case '?': j = parts::QUERY; return true;
-				case '#': j = parts::FRAGMENT; return true;
+				case '/': j = parts::PATH; return (i += i+1 < s.length()), true;
+				case '?': j = parts::QUERY; return (i += i+1 < s.length()), true;
+				case '#': j = parts::FRAGMENT; return (i += i+1 < s.length()), true;
 			}
 
 			return false;
@@ -72,26 +67,22 @@ http_tokenizer::http_tokenizer(const std::string &http_url): m_vct(parts::LENGTH
 			}
 
 			return false;
+		}, [] ([[maybe_unused]] const string &s, [[maybe_unused]] size_t &i, [[maybe_unused]] uint_fast8_t &j) { // match fragment start
+			return false; // NOP
 		}
 	};
 
-	// make a copy
-	auto eval = prototype;
 	uint_fast8_t j = 0;
 
 	// since protocol could be empty and all evaluation from eval
 	// are performed one at time sequentially this is a workaround
 
-	if (http_url.find("://") == std::string::npos) {
-		eval.pop_front();
+	if (http_url.find("://") == std::string::npos)
 		j = parts::HOSTNAME;
-	}
 
 	for (size_t i = 0; i < http_url.length(); ++i) {
-		while ( !eval.empty() && eval.front()(http_url, i, j) )
-			eval.pop_front();
-
-		 m_vct.at(j).put(http_url.at(i));
+		while ( eval.at(j)(http_url, i, j) );
+		m_vct.at(j).put(http_url.at(i));
 	}
 
 }
